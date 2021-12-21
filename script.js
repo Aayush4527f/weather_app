8838bc90a93ccfd7930e4ef41d20320f3b3e5d2a
Aayush <aayushchhajed@gmail.com>
2021-12-21 18:29:06 +0530

    css remaining

---
+++ script.js
@@ -0,0 +1,15 @@
+let APIKEY = "380f2cbe1fbe01de6d82379cea4d7668"
+let arr
+let user_loc = document.getElementById('user_location') 
+
+function getData() {
+    main(user_loc.value)
+}
+
+async function main(place) {
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${APIKEY}`).then(res=>res.json()).then(data=>arr=data)
+    document.getElementById('location_name').innerText = arr.name
+    document.getElementById('temprature').innerText = Math.floor(arr.main.temp - 273.15) + '°C'
+    document.getElementById('wind_speed').innerText = arr.wind.speed + 'm/s'
+    document.getElementById('weather_type').innerText = arr.weather[0].main
+}
