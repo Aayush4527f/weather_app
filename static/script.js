8d3ecb0d067393863bbd67c905545db3902b4728
Aayush <aayushchhajed@gmail.com>
2022-01-04 20:07:29 +0530

    completed

--- static/script.js
+++ static/script.js
@@ -1,8 +1,34 @@
 let arr
 let APIKEY
+let temperature_elem = document.getElementById('temperature')
+let feels_like_elem = document.getElementById('feels_like')
+let pressure_elem = document.getElementById('pressure')
+let sunrise_elem = document.getElementById('sunrise')
+let sunset_elem = document.getElementById('sunset')
+let wind_speed_elem = document.getElementById('wind_speed')
+let visibility_elem = document.getElementById('visibility')
+let location_div = document.getElementById('location')
+
+location_div.addEventListener('keyup',()=>{
+        setTimeout(() => {
+            main(location_div.innerText)
+        }, 50);
+})
+
 
 fetch('/key').then(res=>APIKEY = res.json()).then(data=>APIKEY=data)
 
-function main() {
-    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${APIKEY}`).then(res=>res.json()).then(data=>arr=data)
+async function main(place) {
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${APIKEY}`).then(res=>res.json()).then(data=>arr=data)
+    temperature_elem.innerText = `${Math.floor(arr.main.temp - 273.15)}°C`
+    feels_like_elem.innerText = `${Math.floor(arr.main.feels_like - 273.15)}°C`
+    pressure_elem.innerText = arr.main.pressure
+    sunrise_elem.innerText = arr.sys.sunrise
+    sunset_elem.innerText = arr.sys.sunset
+    wind_speed_elem.innerText = arr.wind.speed
+    visibility_elem.innerText = arr.visibility
 }
+
+setTimeout(() => {
+    main('delhi')
+}, 100);
